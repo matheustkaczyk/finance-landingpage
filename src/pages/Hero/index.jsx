ba9352c283921951445996689ca1570d1be1f701
matheustkaczyk <matheus111904@hotmail.com>
2022-10-03 10:22:10 -0300

    feat: background images configurados

--- src/pages/Hero/index.jsx
+++ src/pages/Hero/index.jsx
@@ -1,11 +1,15 @@
 import Header from "../../components/Header";
 import arrow from '../../assets/arrow-down-icon.svg';
 import heroImg from '../../assets/hero-img.svg';
+import backgroundDark from '../../assets/background-dark-img.svg';
+import background from '../../assets/background-img.svg';
 
 const Hero = () => {
   return (
     <div className="hero">
       <Header />
+      <img src={backgroundDark} alt="background dark" className="background-dark" />
+      <img src={background} alt="background" className="background" />
       <div className="hero-content">
         <div className="hero-infos">
           <h1>Mutual Investment</h1>
