d086b1067074121078c6450649862f21e2c381c9
matheustkaczyk <matheus111904@hotmail.com>
2022-10-03 09:14:09 -0300

    feat: estruturando componente header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,19 @@
+import twitter from '../../assets/twitter-icon.svg';
+import facebook from '../../assets/facebook-icon.svg';
+import medium from '../../assets/medium-icon.svg';
+import logo from '../../assets/logo-icon.svg';
+import hamburger from '../../assets/hamburger-icon.svg';
+
 const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <div>
+        <img src={twitter} alt="twitter logo" />
+        <img src={facebook} alt="facebook logo" />
+        <img src={medium} alt="medium logo" />
+      </div>
+      <img src={logo} alt="Finance logo" />
+      <img src={hamburger} alt="hamburger menu" />
     </header>
   );
 }
