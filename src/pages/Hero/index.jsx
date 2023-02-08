fd6d1a6be612e5c6b441265c958b44494945e35c
matheustkaczyk <matheus111904@hotmail.com>
2023-02-08 09:12:02 -0300

    feat: adding ultra-wide support

--- src/pages/Hero/index.jsx
+++ src/pages/Hero/index.jsx
@@ -8,22 +8,26 @@ const Hero = () => {
   return (
     <div className="hero">
       <Header />
-      <img src={backgroundDark} alt="background dark" className="background-dark" />
-      <img src={background} alt="background" className="background" />
-      <div className="hero-content">
-        <div className="hero-infos">
-          <h1>Mutual Investment</h1>
-          <p>
-            At Finance we care about your future. We help you invest the way you want. So you can relax, have fun and let your fund grow.
-          </p>
-          <button>Learn more</button>
-          <div className="line-wrapper">
-            <hr />
+      <div className="ultrawide-outer-wrapper">
+        <div className="ultrawide-inner-wrapper">
+          <img src={backgroundDark} alt="background dark" className="background-dark" />
+          <img src={background} alt="background" className="background" />
+          <div className="hero-content">
+            <div className="hero-infos">
+              <h1>Mutual Investment</h1>
+              <p>
+                At Finance we care about your future. We help you invest the way you want. So you can relax, have fun and let your fund grow.
+              </p>
+              <button>Learn more</button>
+              <div className="line-wrapper">
+                <hr />
+              </div>
+            </div>
+            <img src={heroImg} alt="Hero finance image" className="hero-image" />
           </div>
+          <img src={arrow} alt="arrow icon" className="hero-arrow-icon" />
         </div>
-        <img src={heroImg} alt="Hero finance image" className="hero-image" />
       </div>
-        <img src={arrow} alt="arrow icon" className="hero-arrow-icon" />
     </div>
   );
 }
